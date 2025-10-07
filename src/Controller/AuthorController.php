47b9ed5b68a5eea5983273b54886b4c5370aaa8a
AlouiOmar97 <omar.aloui@esprit.tn>
2025-10-07 15:33:12 +0100

    crud

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
 use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -48,5 +50,43 @@ final class AuthorController extends AbstractController
         return $this->render('author/details.html.twig',[
             "author" => $author
         ]);
+    }
+
+    #[Route("/author/search/{username}", name:"app_author_search_username")]
+    public function searchAuthorByUsername($username, AuthorRepository $authorRepository): Response{
+        $author= $authorRepository->findOneByUsername($username);
+        //dd($author);
+        return $this->render('author/details.html.twig',[
+            "author" => $author
+        ]);
+
+    }
+
+    #[Route('/author/add/{username}', name:'app_author_add')]
+    public function addAuthor($username, EntityManagerInterface $em): void{
+        $author= new Author();
+        $author->setUsername($username);
+        $author->setEmail('taha.hussein@gmail.com');
+        $author->setPicture('/images/Taha_Hussein.jpg');
+        $author->setNbBooks(300);
+        $em->persist($author);
+        $em->flush();
+        dd($author);
+    }
+
+    #[Route('/author/edit/{id}', name:'app_author_edit')]
+    public function editAuthor($id, EntityManagerInterface $em, AuthorRepository $authorRepository): void{
+        $author= $authorRepository->find($id);
+        $author->setEmail('email edited 22222!');
+        //$em->persist($author);
+        $em->flush();
+        dd($author);
+    }
+    #[Route('/author/delete/{id}', name:'app_author_delete')]
+    public function deleteAuthor($id, EntityManagerInterface $em, AuthorRepository $authorRepository): void{
+        $author = $authorRepository->find($id);
+        $em->remove($author);
+        $em->flush();
+        dd("author deleted");
     }
 }
